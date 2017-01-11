b8846c422ccd7a50b5f36a8efcb2c0307be9a80a
Cody Roseborough <Cody.Roseborough@intel.com>
2017-01-10 16:17:00 -0800

    an update

--- collectd/collectd.go
+++ collectd/collectd.go
@@ -61,37 +61,47 @@ func runMyServer(port int) {
 }
 
 func requestToMetric(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("REQUEST RECEIVED")
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		fmt.Fprintf(w, "err: %v", err)
 		return
 	}
-	fmt.Printf(string(body))
-	//PUTVAL wcp2/swap/swap-used interval=10.000 1484002777.039:0.000000
 
-	s := strings.Split(string(body), "\n")
+	// raw request
+	fmt.Printf(string(body))
+	// Example data format for each line:
+	//PUTVAL wcp2/swap/swap-used interval=10.000 1484002777.039:0.000000
+	rm := strings.Replace(string(body), "\r", "", -1)
+	s := strings.Split(rm, "\n")
 	var mts []plugin.Metric
+	// foreach line, we want to parse it
 	for _, line := range s {
 		if len(line) == 0 {
 			continue
 		}
+		// split on spaces so we can get each section individually
 		sp := strings.Split(line, " ")
 		// Skip PUTVAL
-		//first section is namespace, separated by /
-		namespace := strings.Split(sp[1], "/")
+		//first section is hostname + namespace, separated by /
+		path := strings.Split(sp[1], "/")
+		namespace := path[1:]
+		hostname := path[0]
+		tags := make(map[string]string)
+		tags["hostname"] = hostname
 		// interval=interval
 		// discard interval for now
 		// data is format epoch-time:data
 		dtime := strings.Split(sp[3], ":")
 		data, err := strconv.ParseFloat(dtime[1], 64)
 		if err != nil {
+			fmt.Println("ERROR:", err, "\n\n\n")
 			continue
 		}
 		// last section is time since epoch in seconds
 		seconds := strings.Split(dtime[0], ".")
 		t, err := strconv.ParseInt(seconds[0], 10, 64)
 		if err != nil {
+			fmt.Println("ERROR:", err, "\n\n\n")
 			continue
 		}
 		timestamp := time.Unix(t, 0)
@@ -101,7 +111,9 @@ func requestToMetric(w http.ResponseWriter, r *http.Request) {
 			Timestamp: timestamp,
 			Version:   1,
 			Data:      data,
+			Tags:      tags,
 		}
+		fmt.Println("Adding metric: ", namespace)
 		mts = append(mts, metric)
 	}
 
@@ -118,7 +130,7 @@ func (RelayCollector) GetMetricTypes(cfg plugin.Config) ([]plugin.Metric, error)
 	metrics := []plugin.Metric{}
 
 	metric := plugin.Metric{
-		Namespace: plugin.NewNamespace("collectd"),
+		Namespace: plugin.NewNamespace("snap-relay", "collectd"),
 		Version:   1,
 	}
 	metrics = append(metrics, metric)
